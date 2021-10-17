b2b7b1131443b2627f3a620849863d454a06f514
yingqi0408 <ying_liu@chicagobooth.edu>
2021-10-17 20:51:31 +0000

    lots of update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,52 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
 
   def play_rock
-   # render({:plain => "Howdy, world!"})
+   
+    @comp_move = ["rock","paper","scissors"].sample
+
+    if @comp_move == "rock" 
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end 
+
     render({ :template => "game_templates/user_rock.html.erb"})
   end
-end
+
+  def play_paper
+
+    @comp_move = ["rock","paper","scissors"].sample
+
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end 
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  # def play_scissors
+  #   @comp_move = ["rock", "paper", "scissors"].sample
+
+  #   if @comp_move == "rock"
+  #     @outcome = "lost"
+  #   elsif @comp_move == "paper"
+  #     @outcome = "won"
+  #   elsif @comp_move == "scissors"
+  #     @outcome = "tied"
+  #   end
+
+  #   render({ :template => "game_templates/user_scissors.html.erb"})
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-get("/rock", {:controller => "application", :action => "play_rock"})
 
+  get("/", { :controller => "application", :action => "homepage"})
+
+  get("/rock", {:controller => "application", :action => "play_rock"})
+
+  get("/paper", { :controller => "application", :action => "play_paper"})
+  
+  get("/scissors", { :controller => "application", :action => "play_scissors"})
 end
